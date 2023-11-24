58f43fea4e6045a2fcb825bf43e5160fe880ab94
Brandon J Knight <106040938+BKnightHD@users.noreply.github.com>
2023-11-24 17:53:13 -0600

    Created using Colaboratory

---
+++ Review7.py
@@ -0,0 +1,81 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOvpRWsWWngmQsFTqamhLhw",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/BKnightHD/Python-CC/blob/main/Review7.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "Jt2DGM9kv0uD",
+        "outputId": "73176e1b-5807-43ff-d873-7634904766ab"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "Working in Jupyter notebook, learning the basics\n",
+            "This is going to be fun\n",
+            "...\n",
+            "\n",
+            "2\n",
+            "3\n",
+            "4\n",
+            "5\n",
+            "6\n",
+            "7\n"
+          ]
+        }
+      ],
+      "source": [
+        "message = \"Working in Jupyter notebook, learning the basics\"\n",
+        "message += \"\\nThis is going to be fun\"\n",
+        "print(message)\n",
+        "print('...\\n')\n",
+        "\n",
+        "# Recreating a while loop\n",
+        "\n",
+        "number = 2\n",
+        "while number <=7:\n",
+        "    print(number)\n",
+        "    number += 1"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "vxTYmckZv1SU"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
